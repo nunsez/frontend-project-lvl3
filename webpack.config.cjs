437d04993768d1b1ded71155071bdbfdcecdece3
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-03-07 14:53:20 +0700

    code refactoring v2

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,30 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        stats: 'errors-only',
-    },
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-        new HtmlWebpackPlugin({
-            title: 'RSS Reader',
-            template: 'index.html',
-        }),
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    stats: 'errors-only',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: 'RSS Reader',
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  },
 };
