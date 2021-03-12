20d085c7ea1d393312c3e8f286a0f04c37489e58
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-03-12 14:55:43 +0700

    update eslint rules

--- src/init.js
+++ src/init.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 import axios from 'axios';
 import * as yup from 'yup';
 import i18next from 'i18next';
--- src/view.js
+++ src/view.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 import onChange from 'on-change';
 
 const markAsRead = (element, item) => {
